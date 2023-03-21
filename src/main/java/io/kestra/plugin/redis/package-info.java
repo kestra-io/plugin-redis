e8c12d3ebb82bebe4220bbcb76161430b240dc52
Loïc Mathieu <loikeseke@gmail.com>
2023-03-21 14:36:58 +0100

    feat: plugin categories and icons

---
+++ src/main/java/io/kestra/plugin/redis/package-info.java
@@ -0,0 +1,7 @@
+@PluginSubGroup(
+    description = "This sub-group of plugins contains tasks for using the Redis NoSQL database.",
+    categories = PluginSubGroup.PluginCategory.DATABASE
+)
+package io.kestra.plugin.redis;
+
+import io.kestra.core.models.annotations.PluginSubGroup;
