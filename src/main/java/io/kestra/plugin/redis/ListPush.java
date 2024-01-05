109c12294c88ce1bf619aa524590725eae882be8
Loïc Mathieu <loikeseke@gmail.com>
2024-01-05 10:21:32 +0100

    feat(*): Migrate from RxJava2 to Reactor

--- src/main/java/io/kestra/plugin/redis/ListPush.java
+++ src/main/java/io/kestra/plugin/redis/ListPush.java
@@ -10,18 +10,21 @@ import io.kestra.core.runners.RunContext;
 import io.kestra.core.serializers.FileSerde;
 import io.kestra.core.serializers.JacksonMapper;
 import io.kestra.plugin.redis.models.SerdeType;
-import io.reactivex.BackpressureStrategy;
-import io.reactivex.Flowable;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.*;
 import lombok.experimental.SuperBuilder;
 
 import jakarta.validation.constraints.NotNull;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.FluxSink;
+
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.URI;
 import java.util.Collections;
 import java.util.List;
+
+import static io.kestra.core.utils.Rethrow.throwFunction;
 
 @SuperBuilder
 @ToString
@@ -84,18 +87,18 @@ public class ListPush extends AbstractRedisConnection implements RunnableTask<Li
             if (from instanceof String fromUrl) {
                 URI fromURI = new URI(runContext.render(fromUrl));
                 try (BufferedReader inputStream = new BufferedReader(new InputStreamReader(runContext.uriToInputStream(fromURI)))) {
-                    Flowable<Object> flowable = Flowable.create(FileSerde.reader(inputStream), BackpressureStrategy.BUFFER);
-                    Flowable<Integer> resultFlowable = this.buildFlowable(flowable, runContext, factory);
+                    Flux<Object> flowable = Flux.create(FileSerde.reader(inputStream), FluxSink.OverflowStrategy.BUFFER);
+                    Flux<Integer> resultFlowable = this.buildFlowable(flowable, runContext, factory);
                     count = resultFlowable
                         .reduce(Integer::sum)
-                        .blockingGet();
+                        .block();
                 }
             } else if (from instanceof List<?> fromList) {
-                Flowable<Object> flowable = Flowable.fromArray((fromList).toArray());
-                Flowable<Integer> resultFlowable = this.buildFlowable(flowable, runContext, factory);
+                Flux<Object> flowable = Flux.fromArray((fromList).toArray());
+                Flux<Integer> resultFlowable = this.buildFlowable(flowable, runContext, factory);
                 count = resultFlowable
                     .reduce(Integer::sum)
-                    .blockingGet();
+                    .block();
             }
             else {
                 // should not occur as validation mandates String or List
@@ -107,16 +110,16 @@ public class ListPush extends AbstractRedisConnection implements RunnableTask<Li
         }
     }
 
-    private Flowable<Integer> buildFlowable(Flowable<Object> flowable, RunContext runContext, RedisFactory factory) {
+    private Flux<Integer> buildFlowable(Flux<Object> flowable, RunContext runContext, RedisFactory factory) throws Exception {
         return flowable
-            .map(row -> {
+            .map(throwFunction(row -> {
                 factory.listPush(
                     runContext.render(key),
                     Collections.singletonList(serdeType.serialize(row))
                 );
 
                 return 1;
-            });
+            }));
     }
 
     @Builder
