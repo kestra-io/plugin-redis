01fd3cba9107359118a37574c1d473fb3c7ec271
nKwiatkowski <nkwiatkowski@kestra.io>
2025-04-24 11:27:37 +0200

    test(plugin): fix repository loader

--- src/test/java/io/kestra/plugin/redis/RealtimeTriggerTest.java
+++ src/test/java/io/kestra/plugin/redis/RealtimeTriggerTest.java
@@ -1,25 +1,25 @@
 package io.kestra.plugin.redis;
 
-import io.kestra.core.models.executions.Execution;
-import io.kestra.core.utils.TestsUtils;
-import org.junit.jupiter.api.Test;
-import reactor.core.publisher.Flux;
-
-import java.util.List;
-import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.TimeUnit;
-
 import static io.kestra.core.utils.Rethrow.throwRunnable;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 
+import io.kestra.core.models.executions.Execution;
+import io.kestra.core.utils.TestsUtils;
+import java.util.List;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+import org.junit.jupiter.api.Test;
+import reactor.core.publisher.Flux;
+
 class RealtimeTriggerTest extends AbstractTriggerTest {
+
     @Test
     void flow() throws Exception {
         CountDownLatch queueCount = new CountDownLatch(4);
         Flux<Execution> receive = TestsUtils.receive(executionQueue, execution -> {
-            queueCount.countDown();
             assertThat(execution.getLeft().getFlowId(), is("realtime"));
+            queueCount.countDown();
         });
 
         this.run("realtime.yaml", throwRunnable(() -> {
