80faca85e068962638c687bcea65c9dd24c6f207
Ludovic DEHON <tchiot.ludo@gmail.com>
2025-05-20 09:58:59 +0200

    chore(test): inject main tenants on storage and runOne

--- src/test/java/io/kestra/plugin/redis/ListPushTest.java
+++ src/test/java/io/kestra/plugin/redis/ListPushTest.java
@@ -5,6 +5,7 @@ import io.kestra.core.runners.RunContext;
 import io.kestra.core.runners.RunContextFactory;
 import io.kestra.core.serializers.FileSerde;
 import io.kestra.core.storages.StorageInterface;
+import io.kestra.core.tenant.TenantService;
 import io.kestra.core.utils.IdUtils;
 import io.kestra.plugin.redis.list.ListPush;
 import io.kestra.plugin.redis.string.Delete;
@@ -107,6 +108,6 @@ class ListPushTest {
         for (int i = 0; i < 5; i++) {
             FileSerde.write(output, i);
         }
-        return storageInterface.put(null, null, URI.create("/" + IdUtils.create() + ".ion"), new FileInputStream(tempFile));
+        return storageInterface.put(TenantService.MAIN_TENANT, null, URI.create("/" + IdUtils.create() + ".ion"), new FileInputStream(tempFile));
     }
 }
--- src/test/java/io/kestra/plugin/redis/PublishTest.java
+++ src/test/java/io/kestra/plugin/redis/PublishTest.java
@@ -5,6 +5,7 @@ import io.kestra.core.runners.RunContext;
 import io.kestra.core.runners.RunContextFactory;
 import io.kestra.core.serializers.FileSerde;
 import io.kestra.core.storages.StorageInterface;
+import io.kestra.core.tenant.TenantService;
 import io.kestra.core.utils.IdUtils;
 import io.kestra.plugin.redis.pubsub.Publish;
 import io.kestra.plugin.redis.string.Delete;
@@ -90,6 +91,6 @@ class PublishTest {
         for (int i = 0; i < 5; i++) {
             FileSerde.write(output, i);
         }
-        return storageInterface.put(null, null, URI.create("/" + IdUtils.create() + ".ion"), new FileInputStream(tempFile));
+        return storageInterface.put(TenantService.MAIN_TENANT, null, URI.create("/" + IdUtils.create() + ".ion"), new FileInputStream(tempFile));
     }
 }
