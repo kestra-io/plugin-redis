e5f57300a81557d2e7dfec102bbc287fae56aa74
shubhammenroy <menroy.shubham@gmail.com>
2025-10-25 01:14:57 +0530

    feat: use the new Data.from() facility

--- src/main/java/io/kestra/plugin/redis/list/ListPush.java
+++ src/main/java/io/kestra/plugin/redis/list/ListPush.java
@@ -1,28 +1,19 @@
 package io.kestra.plugin.redis.list;
 
-import io.kestra.core.exceptions.IllegalVariableEvaluationException;
 import io.kestra.core.models.annotations.Example;
 import io.kestra.core.models.annotations.Metric;
 import io.kestra.core.models.annotations.Plugin;
-import io.kestra.core.models.annotations.PluginProperty;
 import io.kestra.core.models.executions.metrics.Counter;
 import io.kestra.core.models.property.Property;
 import io.kestra.core.models.tasks.RunnableTask;
 import io.kestra.core.runners.RunContext;
-import io.kestra.core.serializers.FileSerde;
 import io.kestra.core.serializers.JacksonMapper;
 import io.kestra.plugin.redis.AbstractRedisConnection;
 import io.kestra.plugin.redis.models.SerdeType;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.*;
 import lombok.experimental.SuperBuilder;
-
 import jakarta.validation.constraints.NotNull;
-import reactor.core.publisher.Flux;
-
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.net.URI;
 import java.util.Collections;
 import java.util.List;
 
@@ -77,7 +68,6 @@ public class ListPush extends AbstractRedisConnection implements RunnableTask<Li
         anyOf = {String.class, List.class}
     )
     @NotNull
-    @PluginProperty(dynamic = true)
     private Object from;
 
     @Schema(
@@ -90,10 +80,7 @@ public class ListPush extends AbstractRedisConnection implements RunnableTask<Li
     @Override
     public Output run(RunContext runContext) throws Exception {
         try (RedisFactory factory = this.redisFactory(runContext)) {
-
-            Integer count;
-
-            Object from = null;
+             Object from = null;
             if (this.from instanceof String fromString) {
                 String renderedFrom = runContext.render(fromString);
                 try {
@@ -104,48 +91,19 @@ public class ListPush extends AbstractRedisConnection implements RunnableTask<Li
             } else {
                 from = this.from;
             }
+            Integer count = io.kestra.core.models.property.Data
+                    .from(from).readAs(runContext, String.class, obj -> obj.toString()).map(throwFunction(row -> {
+                factory.getSyncCommands().lpush(
+                    runContext.render(key).as(String.class).orElseThrow(),
+                    Collections.singletonList(runContext.render(serdeType).as(SerdeType.class).orElse(SerdeType.STRING).serialize(runContext.render(row))).toArray(new String[0])
+                );
 
-            if (from instanceof String fromUrl) {
-                URI fromURI = new URI(runContext.render(fromUrl));
-                try (BufferedReader inputStream = new BufferedReader(new InputStreamReader(runContext.storage().getFile(fromURI)))) {
-                    Flux<Object> flowable = FileSerde.readAll(inputStream);
-                    Flux<Integer> resultFlowable = this.buildFlowable(flowable, runContext, factory);
-                    count = resultFlowable.reduce(Integer::sum).blockOptional().orElse(0);
-                }
-            } else if (from instanceof List<?> fromList) {
-                Flux<Object> flowable = Flux.create(objectFluxSink -> {
-                    for (Object o : fromList) {
-                        try {
-                            objectFluxSink.next(runContext.render((String) o));
-                        } catch (Exception e) {
-                            objectFluxSink.error(e);
-                        }
-                    }
-                    objectFluxSink.complete();
-                });
-                Flux<Integer> resultFlowable = this.buildFlowable(flowable, runContext, factory);
-                count = resultFlowable.reduce(Integer::sum).blockOptional().orElse(0);
-            }
-            else {
-                // should not occur as validation mandates String or List
-                throw new IllegalVariableEvaluationException("Invalid 'from' property type :" + from.getClass());
-            }
+                return 1;
+            })).reduce(Integer::sum).blockOptional().orElse(0);
 
             runContext.metric(Counter.of("inserted.records.count", count));
             return Output.builder().count(count).build();
         }
-    }
-
-    private Flux<Integer> buildFlowable(Flux<Object> flowable, RunContext runContext, RedisFactory factory) throws Exception {
-        return flowable
-            .map(throwFunction(row -> {
-                factory.getSyncCommands().lpush(
-                    runContext.render(key).as(String.class).orElseThrow(),
-                    Collections.singletonList(runContext.render(serdeType).as(SerdeType.class).orElse(SerdeType.STRING).serialize(row)).toArray(new String[0])
-                );
-
-                return 1;
-            }));
     }
 
     @Builder
