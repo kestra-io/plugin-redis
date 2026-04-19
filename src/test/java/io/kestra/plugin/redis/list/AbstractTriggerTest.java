6ea982a7014db8d5d2403aceb1d2721fc5b4e4a0
Malay Dewangan <66718045+Malaydewangan09@users.noreply.github.com>
2026-04-19 17:23:11 +0530

    test: use KestraTest scheduler for trigger tests (#158)

--- src/test/java/io/kestra/plugin/redis/list/AbstractTriggerTest.java
+++ src/test/java/io/kestra/plugin/redis/list/AbstractTriggerTest.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 import java.net.URISyntaxException;
 import java.util.Arrays;
 import java.util.Objects;
-import java.util.UUID;
 
 import org.junit.jupiter.api.TestInstance;
 
@@ -16,29 +15,17 @@ import io.kestra.core.models.property.Property;
 import io.kestra.core.queues.QueueFactoryInterface;
 import io.kestra.core.queues.QueueInterface;
 import io.kestra.core.repositories.LocalFlowRepositoryLoader;
-import io.kestra.core.runners.FlowListeners;
 import io.kestra.core.runners.RunContextFactory;
 import io.kestra.core.utils.TestsUtils;
-import io.kestra.jdbc.runner.JdbcScheduler;
-import io.kestra.scheduler.AbstractScheduler;
-import io.kestra.worker.DefaultWorker;
-
-import io.micronaut.context.ApplicationContext;
 import jakarta.inject.Inject;
 import jakarta.inject.Named;
 
 import static io.kestra.core.tenant.TenantService.MAIN_TENANT;
 
-@KestraTest
+@KestraTest(startRunner = true, startScheduler = true)
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 abstract class AbstractTriggerTest {
     protected static final String REDIS_URI = "redis://:redis@localhost:6379/0";
-
-    @Inject
-    private ApplicationContext applicationContext;
-
-    @Inject
-    private FlowListeners flowListenersService;
 
     @Inject
     protected LocalFlowRepositoryLoader repositoryLoader;
@@ -51,17 +38,8 @@ abstract class AbstractTriggerTest {
     protected QueueInterface<Execution> executionQueue;
 
     protected void run(String filename, Runnable runnable) throws IOException, URISyntaxException, InterruptedException {
-        try (
-            AbstractScheduler scheduler = new JdbcScheduler(this.applicationContext, this.flowListenersService);
-            DefaultWorker worker = applicationContext.createBean(DefaultWorker.class, UUID.randomUUID().toString(), 8, null);
-        ) {
-            worker.run();
-            scheduler.run();
-
-            repositoryLoader.load(MAIN_TENANT, Objects.requireNonNull(AbstractTriggerTest.class.getClassLoader().getResource("flows/" + filename)));
-
-            runnable.run();
-        }
+        repositoryLoader.load(MAIN_TENANT, Objects.requireNonNull(AbstractTriggerTest.class.getClassLoader().getResource("flows/" + filename)));
+        runnable.run();
     }
 
     protected String getKey() {
